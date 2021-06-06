5b8bac9ea10ec4167feef969dff47b8cccc2be3b
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2021-06-06 15:02:46 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
-    </div>
+    <>
+      <Typography variant="h1" align="center">
+        React + Next + Material UI Template
+      </Typography>
+      <Typography paragraph> 
+        You can check my components to help in your project <Link href="https://next-material-ui-components.vercel.app/" target="_blank" rel="noopener noreferrer">
+        here
+      </Link>
+      </Typography>
+    </>
   );
 }
